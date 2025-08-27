7d80057717f39eedb3b0fe681701736be4865ce4
Igaruiz <ignacioaruizm96@gmail.com>
2025-08-26 21:58:50 -0300

    Update index.js
    cambios 2

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,16 @@ export default function Home() {
     <div>
       <Navbar />
       <section id="about" className="min-h-screen flex items-center justify-center bg-dark text-white">
-        <h1 className="text-5xl font-bold text-foxyred">La experiencia de citas más 🔥 de internet</h1>
+        <h1 className="text-5xl font-bold text-foxyred">No es solo un chat… es una experiencia.</h1>
       </section>
       <section id="planes" className="min-h-screen flex items-center justify-center bg-black text-white">
         <h2 className="text-3xl">Nuestros Planes</h2>
       </section>
       <section id="extras" className="min-h-screen flex items-center justify-center bg-dark text-white">
-        <h2 className="text-3xl">Extras</h2>
+        <h2 className="text-3xl">Contenido Extra</h2>
       </section>
       <section id="testimonios" className="min-h-screen flex items-center justify-center bg-black text-white">
-        <h2 className="text-3xl">Testimonios</h2>
+        <h2 className="text-3xl">Experiencias</h2>
       </section>
       <footer className="p-6 text-center bg-dark text-white">
         © 2025 Foxy Date. Todos los derechos reservados.
